ddc7dc5fdd4747ac92adcadc8e07613493bba829
FShaaM <zinovevas135@gmail.com>
2026-03-24 18:20:55 +0300

    Added number 2

--- main.cpp
+++ main.cpp
@@ -76,7 +76,70 @@ T* mergeN(const T* const* a, size_t sa, const size_t* sai, T* c)
 }
 
 
+template< class T >
+T* mergeN_basic_warranty(const T* const* a, size_t sa, const size_t* sai, T* c, size_t& size)
+{
+	size_t s = 0;
+	for (size_t i = 0; i < sa; ++i)
+	{
+		s += sai[i];
+	}
 
+	const T** ptr_head = nullptr;
+	const T** ptr_tail = nullptr;
+	try
+	{
+		ptr_head = new const T * [sa];
+		ptr_tail = new const T * [sa];
+
+		for (size_t i = 0; i < sa; ++i)
+		{
+			ptr_head[i] = a[i];
+			ptr_tail[i] = a[i] + sai[i];
+		}
+
+		for (size_t i = 0; i < s; ++i)
+		{
+			const T* min_ptr = nullptr;
+			size_t min_id = 0;
+
+			for (size_t j = 0; j < sa; ++j)
+			{
+				if (ptr_head[j] != ptr_tail[j])
+				{
+					if (!min_ptr)
+					{
+						min_ptr = ptr_head[j];
+						min_id = j;
+					}
+					else
+					{
+						if (*min_ptr > *ptr_head[j])                                    // Оператор сравнения (Может кидать исключения)
+						{
+							min_ptr = ptr_head[j];
+							min_id = j;
+						}
+					}
+				}
+			}
+
+			c[i] = *min_ptr;                                                           // Оператор копирующего присваивания (Может кидать исключения)
+			++size;
+			ptr_head[min_id]++;
+		}
+	}
+	catch (...)
+	{
+		delete[] ptr_head;
+		delete[] ptr_tail;
+		throw;
+	}
+
+	delete[] ptr_head;
+	delete[] ptr_tail;
+
+	return (c + s);
+}
 
 int main()
 {}
